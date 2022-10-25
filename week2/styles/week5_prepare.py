53fbd73db7c5fddb5dc0f7e069bb52f5694451bb
mazzac15 <maz22004@byui.edu>
2022-10-25 14:59:34 -0400

    adding to wwr

---
+++ week2/styles/week5_prepare.py
@@ -0,0 +1,19 @@
+# integer_1=int(input('Give me a number: '))
+# integer_2=int(input('Give me another number: '))
+# if integer_1 > integer_2:
+#     print('The first number is greater.')
+# elif integer_1 < integer_2:
+#     print('The first number is not greater.')
+# elif integer_1 == integer_2:
+#     print('The numbers are equal.')
+# elif integer_2 > integer_1:
+#     print('The second number is not greater.')
+
+print('')
+
+favorite_animal = 'tiger'
+animal=input('What is your favorite animal? ')
+if animal.lower() == favorite_animal:
+    print("That's my favorite animal too!")
+else:
+    print('That one is not my favorite.')
